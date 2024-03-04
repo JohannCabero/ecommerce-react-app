9135865e57ba65da6436703d32030497c0c53cd3
jr.cabero22 <jr.cabero22@gmail.com>
2024-03-04 10:05:33 +0800

    Added descriptions to Highlights cards and fixed vh of home banner

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -8,9 +8,9 @@ export default function Highlights(){
 			<Col className="my-2 my-md-4" xs={12} md={4}>
 				<Card className="highlight">
 					<Card.Body>
-						<Card.Title>Great Deals</Card.Title>
+						<Card.Title as="h4" className="highlight-title">Filipino Made</Card.Title>
 						<Card.Text>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Discover a vibrant collection of products crafted with pride and expertise from the heart of the Philippines and from every corner of the archipelago's thousands of islands. From intricately woven textiles to meticulously made furniture, immerse yourself in the rich cultural heritage and support local enterprises with every purchase.
 						</Card.Text>
 					</Card.Body>
 				</Card>
@@ -19,9 +19,9 @@ export default function Highlights(){
 			<Col className="my-2 my-md-4" xs={12} md={4}>
 				<Card className="highlight">
 					<Card.Body>
-						<Card.Title>Great Deals</Card.Title>
+						<Card.Title as="h4" className="highlight-title">Great Deals</Card.Title>
 						<Card.Text>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Dive into an ocean of savings with our price offerings, where you'll find irresistible discounts on a wide range of products. Whether you're hunting for electronics, fashion, or home essentials, seize the opportunity to snag incredible bargains and elevate your shopping experience without breaking the bank.
 						</Card.Text>
 					</Card.Body>
 				</Card>
@@ -30,9 +30,9 @@ export default function Highlights(){
 			<Col className="my-2 my-md-4" xs={12} md={4}>
 				<Card className="highlight">
 					<Card.Body>
-						<Card.Title>Great Deals</Card.Title>
+						<Card.Title as="h4" className="highlight-title">Quality Selections</Card.Title>
 						<Card.Text>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Elevate your shopping experience with our curated selection of premium products renowned for their exceptional quality and craftsmanship. From luxurious skincare to elegant homeware, indulge in the finer things in life and enhance your lifestyle with every meticulously chosen item.
 						</Card.Text>
 					</Card.Body>
 				</Card>
